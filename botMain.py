2d8bd2289cd139bd7eb97c9f6c39957eb13c39b1
Vincent Chang <frazda@gmail.com>
2017-11-26 17:18:47 +0800

    add logger

--- botMain.py
+++ botMain.py
@@ -1,9 +1,8 @@
-import base64,requests
-from io import BytesIO
+import flask, base64,requests, logging, sys
+
 from myclass.firebaseWrapper import firebaseWrapper
 from myclass.globals import GLOBALS
 from myclass.requestHdlr import requestHdlr, postbackHdlr
-from flask import Flask, request, abort, session, send_file
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -16,39 +15,67 @@ from linebot.models import (
     ImageMessage, TextMessage, 
 )
 
-app          = Flask(__name__)
+app = flask.Flask(__name__)
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.DEBUG)
+
 line_bot_api = LineBotApi(GLOBALS.YOUR_CHANNEL_ACCESS_TOKEN)
 handler      = WebhookHandler(GLOBALS.YOUR_CHANNEL_SECRET)
 
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
+    signature = flask.request.headers['X-Line-Signature']
 
     # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    body = flask.request.get_data(as_text=True)
+    app.logger.info(body)
 
     # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
-        abort(400)
+        flask.abort(400)
 
     return 'OK'
 
+@handler.add(MessageEvent)
+def handle_message(event):
+    app.logger.info('Dispatch event [%s] received at [%s] with token [%s] >>>'%(event.message.type, event.timestamp, event.reply_token))
+    requestHandler = requestHdlr(event, handler, line_bot_api)
+    if event.source.type == 'group' and event.source.group_id in GLOBALS.WHITE_LIST:
+        requestHandler.setWhiteList(True)
+    requestHandler.dispatch()
+
+@handler.add(PostbackEvent)
+def handle_postback(event):
+    app.logger.info('Dispatch [%s] received at [%s] with token [%s] >>>'%(event.postback.data, event.timestamp, event.reply_token))
+    requestHandler = postbackHdlr(event, handler, line_bot_api)
+    if event.source.type == 'group' and event.source.group_id in GLOBALS.WHITE_LIST:
+        requestHandler.setWhiteList(True)
+    requestHandler.dispatch()
+
+@handler.default()
+def default(event):
+    app.logger.info('Drop event [%s] received at [%s] with token [%s]'%(event.type, event.timestamp, event.reply_token))
+
+@app.route("/")
+def helloworld():
+    return 'Hello World'
+
+
 @app.route("/googlemap")
 def send_image():
-    center     = request.args.get('center')
-    markers    = request.args.get('markers')
+    center     = flask.request.args.get('center')
+    markers    = flask.request.args.get('markers')
 
     fb         = firebaseWrapper(GLOBALS.DATABASE_BASE_URL)
-    token      = request.args.get('token')
+    token      = flask.request.args.get('token')
 
     if fb.has_one('funnyBot/gmap_token',token):
         fb.delete_one('funnyBot/gmap_token',token)
     else:
-        return 'Page not found'
+        flask.abort(404)
 
     url = 'https://maps.googleapis.com/maps/api/staticmap'
     params = {
@@ -58,32 +85,15 @@ def send_image():
         'language' : 'zh-tw',
         'markers'  : markers
     }
-    obj_page = requests.get(url, params=params)
+    obj_page = flask.requests.get(url, params=params)
     with open('%s.png'%token, 'wb') as f:
         for chunk in obj_page:
             f.write(chunk)
 
-    return send_file('%s.png'%token, mimetype='image/png')
-
-@handler.add(MessageEvent)
-def handle_message(event):
-    print('[MESSAGE] Dispatch event [%s] received at [%s] with token [%s]'%(event.message.type, event.timestamp, event.reply_token))
-    requestHandler = requestHdlr(event, handler, line_bot_api)
-    if event.source.type == 'group' and event.source.group_id in GLOBALS.WHITE_LIST:
-        requestHandler.setWhiteList(True)
-    requestHandler.dispatch()
-
-@handler.add(PostbackEvent)
-def handle_postback(event):
-    print('[POSTBACK] Dispatch [%s] received at [%s] with token [%s]'%(event.postback.data, event.timestamp, event.reply_token))
-    requestHandler = postbackHdlr(event, handler, line_bot_api)
-    if event.source.type == 'group' and event.source.group_id in GLOBALS.WHITE_LIST:
-        requestHandler.setWhiteList(True)
-    requestHandler.dispatch()
-
-@handler.default()
-def default(event):
-    print('Drop event [%s] received at [%s] with token [%s]'%(event.type, event.timestamp, event.reply_token))
+    return flask.send_file('%s.png'%token, mimetype='image/png')
 
 if __name__ == "__main__":
     app.run()
+
+
+
